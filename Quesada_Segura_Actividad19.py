d36ad2afb8813a45aa29796e61862252a395005b
adrianasegura1507-stack <adriana.segura1507@gmail.com>
2026-05-05 18:13:26 -0500

    ejemplo

--- Quesada_Segura_Actividad19.py
+++ Quesada_Segura_Actividad19.py
@@ -1,4 +1,4 @@
-print("Bienvenido, este es un menú que pregunta al usuario qué quiere hacer y muestra submenús.")
+print("Bienvenido, este es un menú que pregunta al usuario qué quiere hacer y muestra submenús")
 menu = 0
 while menu != 3:
     print("MENÚ PRINCIPAL:")
